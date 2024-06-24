ff57564c33c0751a2fa40f68e0b6f8cd49449806
Alyssa Nicoll <mail@alyssanicoll.com>
2024-06-24 15:34:32 -0500

    update to 18 and ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(), provideClientHydration()]
 };
